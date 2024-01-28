8be6e3a7438c5c8eefb445a893d1c79e284a5755
Selmen Rayen <selmanerayene95@gmail.com>
2024-01-28 05:33:42 +0100

    Update style

--- src/app/components/CreditCard/Form.tsx
+++ src/app/components/CreditCard/Form.tsx
@@ -13,7 +13,7 @@ export default function Form({
 }) {
   return (
     <div>
-      <form className="mt-4 mb-2 max-w-screen-lg flex flex-col gap-1 ml-4">
+      <form className="mt-4 mb-2 max-w-screen-lg flex flex-col gap-1 ml-10">
         <label htmlFor="name">Name</label>
         <input
           id="name"
