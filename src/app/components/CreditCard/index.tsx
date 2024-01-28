ba02c3d67e384b0f2cca9e07e684e8d82642af1c
Selmen Rayen <selmanerayene95@gmail.com>
2024-01-28 23:13:44 +0100

    Add Download pdf

--- src/app/components/CreditCard/index.tsx
+++ src/app/components/CreditCard/index.tsx
@@ -3,11 +3,13 @@
 import Image from "next/image";
 import { Tilt } from "react-tilt";
 import { Share_Tech_Mono } from "next/font/google";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import Form from "./Form";
 import QRCode from "react-qr-code";
 import { HexColorPicker } from "react-colorful";
 import Draggable from "react-draggable";
+import html2canvas from "html2canvas";
+import jsPDF from "jspdf";
 
 const shareTechMono = Share_Tech_Mono({
   weight: "400",
@@ -22,6 +24,34 @@ export type User = {
 };
 
 export const CreditCard = () => {
+  const pdfRef = useRef<HTMLDivElement>(null);
+
+  const downloadPDF = () => {
+    const input = pdfRef.current as HTMLElement;
+
+    html2canvas(input).then((canvas) => {
+      const imgData = canvas.toDataURL("image/png");
+      const pdf = new jsPDF("p", "mm", "a4", true);
+      const pdfWidth = pdf.internal.pageSize.getWidth();
+      const pdfHeight = pdf.internal.pageSize.getHeight();
+      const imgWidth = canvas.width;
+      const imgHeight = canvas.height;
+
+      const ratio = Math.min(pdfWidth / imgWidth, pdfHeight / imgHeight);
+      const imgX = (pdfWidth - imgWidth * ratio) / 2;
+      const imgY = 30;
+      pdf.addImage(
+        imgData,
+        "PNG",
+        imgX,
+        imgY,
+        imgWidth * ratio,
+        imgHeight * ratio
+      );
+      pdf.save(`${user.name} card.pdf`);
+    });
+  };
+
   const [user, setUser] = useState<User>({
     name: "Rayen Selmen",
     email: "selmanerayene95@example.com",
@@ -37,7 +67,10 @@ export const CreditCard = () => {
       <Tilt
         className={`${shareTechMono.className} w-[425px] h-[270px] bg-gradient-to-tr rounded-2xl flex content-center items-center justify-center `}
       >
-        <div className="mx-4 p-4  md:p-8 w-full h-full bg-[url('/bg-1.png')] rounded-3xl">
+        <div
+          className="mx-4 p-4  md:p-8 w-full h-full bg-[url('/bg-1.png')] rounded-3xl"
+          ref={pdfRef}
+        >
           <div className="relative w-full h-full">
             <div className="flex gap-10">
               <Image
@@ -88,13 +121,19 @@ export const CreditCard = () => {
         </div>
       </Tilt>
       <Form setUser={setUser} user={user} />
-
       <HexColorPicker
         className="mx-auto"
         color={color}
         onChange={setColor}
         style={{ width: "360px", height: "150px" }}
       />
+
+      <button
+        className="btn btn-outline btn-primary mx-14 w-72 mt-2"
+        onClick={downloadPDF}
+      >
+        Download PDF
+      </button>
     </div>
   );
 };
