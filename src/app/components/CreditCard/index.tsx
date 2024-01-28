6fb3401917eb8a77c1cee1226a20343fc71e5100
Selmen Rayen <selmanerayene95@gmail.com>
2024-01-28 21:22:56 +0100

    Update

--- src/app/components/CreditCard/index.tsx
+++ src/app/components/CreditCard/index.tsx
@@ -7,6 +7,7 @@ import { useState, useEffect, useRef } from "react";
 import Form from "./Form";
 import QRCode from "react-qr-code";
 import { HexColorPicker, HexColorInput } from "react-colorful";
+import Draggable from "react-draggable";
 
 const shareTechMono = Share_Tech_Mono({
   weight: "400",
@@ -22,8 +23,8 @@ export type User = {
 
 export const CreditCard = () => {
   const [user, setUser] = useState<User>({
-    name: "John Doe",
-    email: "johndoe@example.com",
+    name: "Rayen Selmen",
+    email: "selmanerayene95@example.com",
     phoneNumber: "+1234567890",
     profession: "Software Engineer",
   });
@@ -36,43 +37,42 @@ export const CreditCard = () => {
       <Tilt
         className={`${shareTechMono.className} w-[425px] h-[270px] bg-gradient-to-tr rounded-2xl flex content-center items-center justify-center `}
       >
-        <div className="mx-4 p-4  md:p-8 w-full h-full bg-[url('/bg-1.png')] rounded-3xl">
-          <div className="relative w-full h-full">
-            <div className="flex gap-10">
-              <Image
-                className="absolute"
-                alt=""
-                src="/logo.png"
-                width={75}
-                height={24}
-              />
-            </div>
-
-            <div
-              style={{ color: color }}
-              className={`flex flex-col w-full h-full justify-end gap-4`}
-            >
-              <div className="w-16 ml-80 md:w-16 h-8 md:ml-72">
-                <QRCode
-                  size={256}
-                  style={{ height: "auto", maxWidth: "100%", width: "100%" }}
-                  value={`name:${user.name}\nemail:${user.email}\nphoneNumber:${user.phoneNumber}\nprofession:${user.profession}\n\nCreated By: SRayen`}
-                  viewBox={`0 0 256 256`}
+        <Draggable>
+          <div className="mx-4 p-4  md:p-8 w-full h-full bg-[url('/bg-1.png')] rounded-3xl">
+            <div className="relative w-full h-full">
+              <div className="flex gap-10">
+                <Image
+                  className="absolute"
+                  alt=""
+                  src="/logo.png"
+                  width={75}
+                  height={24}
                 />
               </div>
-              <p className="text-2xl font-bold italic">{user.profession}</p>
-              <p className="text-2xl font-bold ">{user.phoneNumber}</p>
-              <div className="flex gap-9">
-                <div
-                  className="text-lg"
-                  dangerouslySetInnerHTML={{ __html: user.name }}
-                />
-                {/* <p className="text-lg uppercase font-extrabold ">{name}</p> */}
-                <p className="text-lg ">{user.email}</p>
+
+              <div
+                style={{ color: color }}
+                className={`flex flex-col w-full h-full justify-end gap-4`}
+              >
+                <div className="w-16 ml-80 md:w-16 h-8 md:ml-72">
+                  <QRCode
+                    size={256}
+                    style={{ height: "auto", maxWidth: "100%", width: "100%" }}
+                    value={`name:${user.name}\nemail:${user.email}\nphoneNumber:${user.phoneNumber}\nprofession:${user.profession}\n\nCreated By: SRayen`}
+                    viewBox={`0 0 256 256`}
+                  />
+                </div>
+                <p className="text-2xl font-bold italic">{user.profession}</p>
+                <p className="text-2xl font-bold ">{user.phoneNumber}</p>
+                <div className="flex gap-9">
+                  <p className="text-lg  font-bold">{user.name}</p>
+                  {/* <p className="text-lg uppercase font-extrabold ">{name}</p> */}
+                  <p className="text-lg ">{user.email}</p>
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        </Draggable>
       </Tilt>
       <Form setUser={setUser} user={user} />
 
